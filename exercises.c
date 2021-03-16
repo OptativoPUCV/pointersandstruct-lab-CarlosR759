19f03783f295e4c1d77b2d860031058ed8a5e90a
Replit user <replituser@example.com>
2021-03-16 21:41:38 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -127,6 +127,7 @@ la cual retorna el valor en la posición i del vector v.
 int obtenerValor(Vector * v, int i) {
   int resultado;
   resultado = v->datos[i];
+  printf("Resultado: %d\n", resultado);
 
    return resultado;;
 }
