24e2a986d567adfd597fd39eb893e58570cad534
Replit user <replituser@example.com>
2021-03-16 19:12:54 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -39,7 +39,7 @@ Utilice la función sumaN.
 
 void sumaNultimos(int a[], int n, int m, int * suma) {
   int posicionInicialDeSuma = n - m;
-  
+  (*suma) = 0;
   (*suma) = sumaN( &a[posicionInicialDeSuma], n);
       
   return;
